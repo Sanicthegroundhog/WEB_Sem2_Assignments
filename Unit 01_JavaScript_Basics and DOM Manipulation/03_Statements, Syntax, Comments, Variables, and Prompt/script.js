07718fef844efdb26647f8cb0f45bfbc0fcfb19b
Jacob Atterson <atterjac000@warren.k12.in.us>
2024-01-11 08:25:51 -0500

    Java 2-3

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/03_Statements, Syntax, Comments, Variables, and Prompt/script.js
@@ -0,0 +1,9 @@
+let firstName = prompt("Enter your name");
+console.log(firstName);
+
+let lastName = prompt("Enter your name");
+console.log(lastName);
+
+let fullName = firstName + lastName;
+let name = (fullName);
+document.getElementById("name").innerHTML = fullName;
