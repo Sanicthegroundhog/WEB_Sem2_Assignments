ce59a40eae4f5b05f2e378a5cb95f13e7b4d2cf8
Jacob Atterson <atterjac000@warren.k12.in.us>
2024-02-23 08:24:28 -0500

    Unit 6

---
+++ Unit 07_JavaScript Control Structures/01_Random and Comparison/script.js
@@ -0,0 +1,21 @@
+function Roll(){
+    var diceElement = document.getElementById("dice");
+    // diceElement.src = "images/dice2.jpg";
+    let random = Math.random() * 6 + 1;
+    random = Math.trunc(random)
+    // console.log(random);
+
+    if(random === 1){
+        diceElement.src = "images/dice1.jpg";
+    } else if(random === 2){
+        diceElement.src = "images/dice2.jpg"
+    } else if(random === 3){
+        diceElement.src = "images/dice3.jpg"
+    } else if(random === 4){
+        diceElement.src = "images/dice4.jpg"
+    } else if(random === 5){
+        diceElement.src = "images/dice5.jpg"
+    } else if(random === 6){
+        diceElement.src = "images/dice6.jpg"
+    }
+}
