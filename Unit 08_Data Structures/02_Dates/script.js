ef30162daa393a9f0b95c751baf03eeceb116f64
Jacob Atterson <atterjac000@warren.k12.in.us>
2024-03-06 08:57:51 -0500

    Unit 8

---
+++ Unit 08_Data Structures/02_Dates/script.js
@@ -0,0 +1,30 @@
+const currentDate = new Date();
+const month = currentDate.getMonth();
+
+switch(month){
+    case 1:
+        document.getElementById("month").innerHTML = "January";
+        break;
+    case 2:
+        document.getElementById("month").innerHTML = "January";
+        break;
+    case 3:
+        document.getElementById("month").innerHTML = "January";
+        break;
+    case 4:
+        document.getElementById("month").innerHTML = "January";
+        break;
+    case 5:
+        document.getElementById("month").innerHTML = "January";
+        break;
+    case 0:
+        document.getElementById("month").innerHTML = "January";
+        break;
+    case 0:
+        document.getElementById("month").innerHTML = "January";
+        break;
+    case 0:
+        document.getElementById("month").innerHTML = "January";
+                break;
+
+}
